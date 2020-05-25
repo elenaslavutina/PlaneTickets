0f93968025efd367c87373db024bbff622714425
elenaslavutina <elena.slavutina2013@gmail.com>
2020-05-25 17:35:09 +0300

    Added tests for negative price and duration and removing

--- src/test/java/ru/netology/manager/TicketsManagerTest.java
+++ src/test/java/ru/netology/manager/TicketsManagerTest.java
@@ -28,7 +28,7 @@ class TicketsManagerTest {
 
     }
 
-    @Test
+/*    @Test
     public void shouldAddAllProductsToRepository() {
 
         Ticket[] actual = repository.findAll();
@@ -37,7 +37,7 @@ class TicketsManagerTest {
         assertEquals(expectedCount, actualCount);
 
     }
-
+*/
     @Test
     public void shouldFilterAndSort() {
         Ticket[] expected = new Ticket[]{forth, first, second};
